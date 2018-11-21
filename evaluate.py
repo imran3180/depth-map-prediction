8c6fe3ba87e0373651a583f8e30db13c9cf4c75c
diksha meghwal <dm4511@nyu.edu>
2018-11-20 20:02:43 -0500

    fixed minor changes

--- evaluate.py
+++ evaluate.py
@@ -36,45 +36,47 @@ coarse_state_dict = torch.load("models/" + args.model_folder + "/coarse_model_" 
 fine_state_dict = torch.load("models/" + args.model_folder + "/fine_model_" + str(args.model_no) + ".pth")
 
 coarse_model = coarseNet()
+coarse_model.cuda()
 fine_model = fineNet()
+fine_model.cuda()
+
+dtype=torch.cuda.FloatTensor
 
 coarse_model.load_state_dict(coarse_state_dict)
 fine_model.load_state_dict(fine_state_dict)
 coarse_model.eval()
 fine_model.eval()
 
-coarse_model.cuda()
-fine_model.cuda()
-dtype=torch.cuda.FloatTensor
-
 from data import rgb_data_transforms, depth_data_transforms, input_for_plot_transforms
 test_rgb_loader = torch.utils.data.DataLoader(datasets.ImageFolder(args.data + '/test_images/rgb/', transform = rgb_data_transforms), batch_size=args.batch_size, shuffle=False, num_workers=1)
 test_depth_loader = torch.utils.data.DataLoader(datasets.ImageFolder(args.data + '/test_images/depth/', transform = depth_data_transforms), batch_size=args.batch_size, shuffle=False, num_workers=1)
 input_for_plot_loader = torch.utils.data.DataLoader(datasets.ImageFolder(args.data + '/test_images/rgb/', transform = input_for_plot_transforms), batch_size=args.batch_size, shuffle=False, num_workers=1)
 
-def plot_grid(fig, plot_input, coarse_output, fine_output, actual_output):
-	grid = ImageGrid(fig, 141, nrows_ncols=(args.batch_size, 4), axes_pad=0.05, label_mode="1")
-	for i in range(8):
+def plot_grid(fig, plot_input, coarse_output, fine_output, actual_output, row_no):
+	grid = ImageGrid(fig, 141, nrows_ncols=(row_no, 4), axes_pad=0.05, label_mode="1")
+	for i in range(row_no):
 		for j in range(4):
 			if(j == 0):
 				grid[i*4+j].imshow(np.transpose(plot_input[i], (1, 2, 0)), interpolation="nearest")
 			if(j == 1):
-				grid[i*4+j].imshow(np.transpose(coarse_output[i][0].detach().numpy(), (0, 1)), interpolation="nearest")
+				grid[i*4+j].imshow(np.transpose(coarse_output[i][0].detach().cpu().numpy(), (0, 1)), interpolation="nearest")
 			if(j == 2):
-				grid[i*4+j].imshow(np.transpose(fine_output[i][0].detach().numpy(), (0, 1)), interpolation="nearest")
+				grid[i*4+j].imshow(np.transpose(fine_output[i][0].detach().cpu().numpy(), (0, 1)), interpolation="nearest")
 			if(j == 3):
-				grid[i*4+j].imshow(np.transpose(actual_output[i][0].detach().numpy(), (0, 1)), interpolation="nearest")
+				grid[i*4+j].imshow(np.transpose(actual_output[i][0].detach().cpu().numpy(), (0, 1)), interpolation="nearest")
 
 batch_idx = 0
-for batch_idx, (rgb, depth, plot_input) in enumerate(zip(test_rgb_loader, test_depth_loader, input_for_plot_loader)):
-    rgb, depth, plot_input = Variable(rgb[0].cuda(), requires_grad = False), Variable(depth[0].cuda(), requires_grad = False), Variable(plot_input[0], requires_grad = False)
-    coarse_output = coarse_model(rgb.type)
-    fine_output = fine_model(rgb, coarse_output)
-    actual_output = depth[:,0,:,:].view(args.batch_size, 1, output_height, output_width)
-    batch_idx = batch_idx + 1
+for batch_idx,(rgb, depth, plot_input) in enumerate(zip(test_rgb_loader, test_depth_loader, input_for_plot_loader)):
+    rgb, depth, plot_input = Variable(rgb[0].cuda(), requires_grad = False), Variable(depth[0].cuda(), requires_grad = False), Variable(plot_input[0].cuda(), requires_grad = False)
+    print('rgb size:{} depth size:{}'.format(rgb.size(), depth.size()))
+    coarse_output = coarse_model(rgb)
+    fine_output = fine_model(rgb.type(dtype), coarse_output)
+    depth_dim = list(depth.size())
+    actual_output = depth[:,0,:,:].view(depth_dim[0], 1, output_height, output_width)
     F = plt.figure(1, (30, 60))
     F.subplots_adjust(left=0.05, right=0.95)
-    plot_grid(F, plot_input, coarse_output, fine_output, actual_output)
-    plt.savefig("plots/" + args.model_folder + "_" + str(args.model_no) + ".pdf")
+    plot_grid(F, plot_input, coarse_output, fine_output, actual_output, depth_dim[0])
+    plt.savefig("plots/" + args.model_folder + "_" + str(args.model_no) + "_" + str(batch_idx) + ".pdf")
     plt.show()
+    #batch_idx = batch_idx + 1
     #if batch_idx == 1: break
--- main.py
+++ main.py
@@ -48,9 +48,9 @@ val_depth_loader = torch.utils.data.DataLoader(datasets.ImageFolder(args.data + 
 
 from model import coarseNet, fineNet
 coarse_model = coarseNet()
-# coarse_model.cuda()
+coarse_model.cuda()
 fine_model = fineNet()
-# fine_model.cuda()
+fine_model.cuda()
 loss_function = nn.MSELoss()
 
 coarse_optimizer = optim.SGD(coarse_model.parameters(), lr=args.lr, momentum=args.momentum)
@@ -63,13 +63,11 @@ def train_coarse(epoch):
     coarse_model.train()
     batch_idx = 0
     for batch_idx, (rgb, depth) in enumerate(zip(train_rgb_loader, train_depth_loader)):
-        # print('rgb size:{} depth size:{}'.format(len(rgb), len(depth)))
-        #rgb, depth = Variable(rgb[0].cuda()), Variable(depth[0].cuda())
-        rgb, depth = Variable(rgb[0]), Variable(depth[0])
-        # print('rgb size:{} depth size:{}'.format(rgb.size(), depth.size()))
+        rgb, depth = Variable(rgb[0].cuda()), Variable(depth[0].cuda())
+        #rgb, depth = Variable(rgb[0]), Variable(depth[0])
         coarse_optimizer.zero_grad()
-        # output = coarse_model(rgb.type(dtype))
-        output = coarse_model(rgb)
+        output = coarse_model(rgb.type(dtype))
+        #output = coarse_model(rgb)
         loss = loss_function(output, depth[:,0,:,:].view(args.batch_size, 1, output_height, output_width))
         loss.backward()
         coarse_optimizer.step()
@@ -80,22 +78,20 @@ def train_coarse(epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(rgb), len(train_rgb_loader.dataset),
                 100. * batch_idx / len(train_rgb_loader), loss.item()))
-        batch_idx = batch_idx + 1
-        # if batch_idx == 1: break
+        #batch_idx = batch_idx + 1
+        #if batch_idx == 1: break
 
 def train_fine(epoch):
     fine_model.train()
     batch_idx = 0
-    for (rgb, depth) in zip(train_rgb_loader, train_depth_loader):
-        # print('rgb size:{} depth size:{}'.format(len(rgb), len(depth)))
-        rgb, depth = Variable(rgb[0]), Variable(depth[0])
-        # print('rgb size:{} depth size:{}'.format(rgb.size(), depth.size()))
-        # rgb, depth = Variable(rgb[0].cuda()), Variable(depth[0].cuda())
+    for batch_idx,(rgb, depth) in enumerate(zip(train_rgb_loader, train_depth_loader)):
+        #rgb, depth = Variable(rgb[0]), Variable(depth[0])
+        rgb, depth = Variable(rgb[0].cuda()), Variable(depth[0].cuda())
         fine_optimizer.zero_grad()
-        coarse_output = coarse_model(rgb)   # it should print last epoch error since coarse is fixed.
-        output = fine_model(rgb, coarse_output)
-        # coarse_output = coarse_model(rgb.type(dtype))   # it should print last epoch error since coarse is fixed.
-        # output = fine_model(rgb.type(dtype), coarse_output.type(dtype))
+        #coarse_output = coarse_model(rgb)   # it should print last epoch error since coarse is fixed.
+        #output = fine_model(rgb, coarse_output)
+        coarse_output = coarse_model(rgb.type(dtype))   # it should print last epoch error since coarse is fixed.
+        output = fine_model(rgb.type(dtype), coarse_output.type(dtype))
         loss = loss_function(output, depth[:,0,:,:].view(args.batch_size, 1, output_height, output_width))
         loss.backward()
         fine_optimizer.step()
@@ -106,47 +102,53 @@ def train_fine(epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(rgb), len(train_rgb_loader.dataset),
                 100. * batch_idx / len(train_rgb_loader), loss.item()))
-        batch_idx = batch_idx + 1
-        # if batch_idx == 1: break
+        #batch_idx = batch_idx + 1
+        #iif batch_idx == 1: break
 
-def validation():
+coarse_output = torch.tensor([[0]])
+
+def coarse_validation():
     coarse_model.eval()
-    fine_model.eval()
     coarse_validation_loss = 0
+    batch_idx = 0
+    for batch_idx,(rgb, depth) in enumerate(zip(train_rgb_loader, train_depth_loader)):
+        #rgb, depth = Variable(rgb[0], requires_grad = False), Variable(depth[0], requires_grad = False)
+        #coarse_output = coarse_model(rgb)
+        rgb, depth = Variable(rgb[0].cuda(), requires_grad = False), Variable(depth[0].cuda(), requires_grad = False)
+        coarse_output = coarse_model(rgb.type(dtype))
+        coarse_validation_loss += loss_function(coarse_output, depth[:,0,:,:].view(args.batch_size, 1, output_height, output_width)).item()
+        #fine_output = fine_model(rgb, coarse_output)
+        #batch_idx = batch_idx + 1
+        #if batch_idx == 2: break
+    coarse_validation_loss /= batch_idx
+    logger.scalar_summary("coarse validation loss", coarse_validation_loss, epoch)
+    print('\nValidation set: Average loss(Coarse): {:.4f} \n'.format(coarse_validation_loss))
+
+def fine_validation():
+    fine_model.eval()
     fine_validation_loss = 0
     batch_idx = 0
-    for(rgb, depth) in zip(train_rgb_loader, train_depth_loader):
-        # print('rgb size:{} depth size:{}'.format(len(rgb), len(depth)))
-        rgb, depth = Variable(rgb[0], requires_grad = False), Variable(depth[0], requires_grad = False)
-        # print('rgb size:{} depth size:{}'.format(rgb.size(), depth.size()))
-        coarse_output = coarse_model(rgb)
-        # rgb, depth = Variable(rgb[0].cuda(), requires_grad = False), Variable(depth[0].cuda(), requires_grad = False)
-        # coarse_output = coarse_model(rgb.type(dtype))
-        coarse_validation_loss += loss_function(coarse_output, depth[:,0,:,:].view(args.batch_size, 1, output_height, output_width)).item()
-        fine_output = fine_model(rgb, coarse_output)
-        # fine_output = fine_model(rgb.type(dtype), coarse_output)
+    for batch_idx,(rgb, depth) in enumerate(zip(train_rgb_loader, train_depth_loader)):
+        rgb, depth = Variable(rgb[0].cuda(), requires_grad = False), Variable(depth[0].cuda(), requires_grad = False)
+        fine_output = fine_model(rgb.type(dtype), coarse_output)
         fine_validation_loss += loss_function(fine_output, depth[:,0,:,:].view(args.batch_size, 1, output_height, output_width)).item()
-        batch_idx = batch_idx + 1
-        # if batch_idx == 2: break
-    coarse_validation_loss /= batch_idx
     fine_validation_loss /= batch_idx
-    logger.scalar_summary("coarse validation loss", coarse_validation_loss, epoch)
     logger.scalar_summary("fine validation loss", fine_validation_loss, epoch)
-    # logger.scalar_summary("accuracy", 100. * correct / len(val_loader.dataset), epoch)
-    print('\nValidation set: Average loss(Coarse): {:.4f}, Average loss(Fine): {:.4f} \n'.format(
-        coarse_validation_loss, fine_validation_loss))
+    print('\nValidation set: Average loss(Fine): {:.4f} \n'.format(fine_validation_loss))
 
+folder_name = "models/" + args.model_folder
+if not os.path.exists(folder_name): os.mkdir(folder_name)
 
 for epoch in range(1, args.epochs + 1):
     print("********* Training the Coarse Model **************")
     train_coarse(epoch)
-    print("********* Training the Fine Model ****************")
-    train_fine(epoch)
-    validation()
-
-    folder_name = "models/" + args.model_folder
-    if not os.path.exists(folder_name): os.mkdir(folder_name)
+    coarse_validation()
     model_file = folder_name + "/" + 'coarse_model_' + str(epoch) + '.pth'
     torch.save(coarse_model.state_dict(), model_file)
+
+for epoch in range(1, args.epochs + 1):
+    print("********* Training the Fine Model ****************")
+    train_fine(epoch)
+    fine_validation()
     model_file = folder_name + "/" + 'fine_model_' + str(epoch) + '.pth'
     torch.save(fine_model.state_dict(), model_file)
