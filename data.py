de86b33d53753a8b6475265f9c2e9686c8c786d4
Imran <ii398@nyu.edu>
2018-12-11 12:14:30 -0500

    2014 working on best level

--- data.py
+++ data.py
@@ -15,9 +15,9 @@ class TransposeDepthInput(object):
         depth = depth.transpose((2, 0, 1))
         depth = torch.from_numpy(depth)
         depth = depth.view(1, depth.shape[0], depth.shape[1], depth.shape[2])
-        depth = nn.functional.interpolate(depth, size=(55, 74), mode='bilinear', align_corners=False)
-        depth = torch.log(depth[0])
-        return depth
+        depth = nn.functional.interpolate(depth, size = (55, 74), mode='bilinear', align_corners=False)
+        depth = torch.log(depth)
+        return depth[0]
 
 rgb_data_transforms = transforms.Compose([
     transforms.Resize((228, 304)),    # Different for Input Image & Depth Image
@@ -33,55 +33,40 @@ input_for_plot_transforms = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-class RGBDataset(Dataset):
-    def __init__(self, filename, type, transform = None):
+class NYUDataset(Dataset):
+    def __init__(self, filename, type, rgb_transform = None, depth_transform = None):
         f = h5py.File(filename, 'r')
         if type == "training":
-            self.images = f['images'][:1024]
+            # self.images = f['images'][0:1024]
+            # self.depths = f['depths'][0:1024]
+            self.images = f['images'][0:1024]
+            self.depths = f['depths'][0:1024]
         elif type == "validation":
             self.images = f['images'][1024:1248]
+            self.depths = f['depths'][1024:1248]
+            # self.images = f['images'][1024:1072]
+            # self.depths = f['depths'][1024:1072]
         elif type == "test":
             self.images = f['images'][1248:]
-        self.transform = transform
+            self.depths = f['depths'][1248:]
+            # self.images = f['images'][0:32]
+            # self.depths = f['depths'][0:32]
+        self.rgb_transform = rgb_transform
+        self.depth_transform = depth_transform
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
         image = self.images[idx]
-        # pdb.set_trace()
         image = image.transpose((2, 1, 0))
         image = Image.fromarray(image)
-        if self.transform:
-            image = self.transform(image)
-        return image
-
-class DepthDataset(Dataset):
-    def __init__(self, filename, type, transform = None):
-        f = h5py.File(filename, 'r')
-        if type == "training":
-            self.depths = f['depths'][:1024]
-        elif type == "validation":
-            self.depths = f['depths'][1024:1248]
-        elif type == "test":
-            self.depths = f['depths'][1248:]
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.depths)
-
-    def __getitem__(self, idx):
+        if self.rgb_transform:
+            image = self.rgb_transform(image)
         depth = self.depths[idx]
         depth = np.reshape(depth, (1, depth.shape[0], depth.shape[1]))
         depth = depth.transpose((2, 1, 0))
-        if self.transform:
-            depth = self.transform(depth)
-        return depth
-
-# obj = DepthDataset('nyu_depth_v2_labeled.mat', 'training', transform = depth_data_transforms)
-# obj[0]
-
-# class TransposeRGBInput(object):
-#     def __call__(self, image):
-#         image = image.transpose((2, 1, 0))
-#         return torch.from_numpy(image)
+        if self.depth_transform:
+            depth = self.depth_transform(depth)
+        sample = {'image': image, 'depth': depth}
+        return sample
